7be5b9a407c9baf8d9ae18f140ffda616ebab350
theo <theo.kossoy@gmail.com>
2022-01-25 23:58:01 +0300

    feat: add 26_removeDuplicates

---
+++ 26/removeDuplicates.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func removeDuplicates(nums []int) int {
+	k := 0
+	ln := len(nums)
+	for i := 0; i < ln; i++ {
+		if i != 0 {
+			if nums[i] == nums[i-1] {
+				continue
+			}
+			nums[k] = nums[i]
+		}
+		k += 1
+	}
+	return k
+}
+
+func main() {
+	array := []int{1, 1, 1, 2}
+	fmt.Println(removeDuplicates(array))
+}
